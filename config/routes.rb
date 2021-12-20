fecb5c4674b92f597fe2b32e03e1f11b228f2b2d
yuhi-ta <yuhi1307@gmail.com>
2021-12-21 06:54:57 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
   get 'messages/index'
   get 'message/index'
   root to: "messages#index"
